1d9a5720d054ab0af1ca2f37fc2f6f516a56cbbc
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-22 09:34:38 +0100

    added links to github and netlify

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">WEATHER APP</div>;
+  return (
+    <div className="App">
+      WEATHER APP
+      <footer>
+        This project was coded by Chimdinma Egbuchulam and is
+        <a href="https://github.com/didichulam/react-weather-app">
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://dinma-react-weather-app.netlify.app/">Netlify</a>
+      </footer>
+    </div>
+  );
 }
