a61b94a6cc069810da60504e90b4558ec967bf67
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-22 10:16:21 +0100

    added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      WEATHER APP
-      <footer>
-        This project was coded by Chimdinma Egbuchulam and is
-        <a href="https://github.com/didichulam/react-weather-app">
-          open-sourced on GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://dinma-react-weather-app.netlify.app/">Netlify</a>
-      </footer>
+      <div className="container">
+        <h1>WEATHER APP</h1>
+        <footer>
+          This project was coded by Chimdinma Egbuchulam and is{" "}
+          <a href="https://github.com/didichulam/react-weather-app">
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dinma-react-weather-app.netlify.app/">Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
