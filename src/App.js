e2f76e2e7bf2eff3800b4848a0aa32bffc8cf9b8
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-23 19:20:32 +0100

    added "_blank" to the relevant links

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ export default function App() {
         <Weather defaultCity="Birmingham" />
         <footer>
           This project was coded by Chimdinma Egbuchulam and is{" "}
-          <a href="https://github.com/didichulam/react-weather-app">
+          <a
+            href="https://github.com/didichulam/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             open-sourced on GitHub
           </a>{" "}
           and hosted on{" "}
-          <a href="https://dinma-react-weather-app.netlify.app/">Netlify</a>
+          <a
+            href="https://dinma-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
         </footer>
       </div>
     </div>
