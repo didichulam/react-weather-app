9d47017f946d8142c7fe7800f12124d6b96e383d
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-26 18:35:18 +0100

    added forecast for the first day

---
+++ src/WeatherDailyForecast.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function WeatherDailyForecast(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="WeatherForecast-day">{day()}</div>
+      <img
+        src={props.data.condition.icon_url}
+        alt={props.data.condition.icon}
+      />{" "}
+      <div className="WeatherForecast-temperatures">
+        <span className="WeatherForecast-temperatures-max">
+          {maxTemperature()}
+        </span>
+        <span className="WeatherForecast-temperatures-min">
+          {minTemperature()}
+        </span>
+      </div>
+    </div>
+  );
+}
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherDailyForecast from "./WeatherDailyForecast";
 
-export default function WeatherForecast() {
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div classname="WeatherForecast-day">Thur</div>
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-            alt="weather-description"
-          />{" "}
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperatures-max">19°</span>
-            <span className="WeatherForecast-temperatures-min">10°</span>
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherDailyForecast data={forecast.daily[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "fc07ae9o4bd8db7562f510t9323275bb";
+    let city = props.data.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
