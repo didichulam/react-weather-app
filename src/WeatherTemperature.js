025de566c7c75543b17f5f229cc8b8e577a058a7
didichulam <140446190+didichulam@users.noreply.github.com>
2024-07-29 17:11:58 +0100

    add useEffect to change forecast when city changes

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelcius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celcius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celcius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" className="text-decoration-none" onClick={showFahrenheit}>
-            °F
-          </a>{" "}
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit())}</span>
-        <span className="unit">
-          <a href="/" className="text-decoration-none" onClick={showCelcius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celcius)}</span>
+      <span className="unit">°C</span>
+    </span>
+  );
 }
