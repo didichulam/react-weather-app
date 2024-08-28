386eec5db9795d24fcd0a1af94aba609dbcb80b3
didichulam <140446190+didichulam@users.noreply.github.com>
2024-08-28 18:32:11 +0100

    added some styling

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultCity="Birmingham" />
-        <footer>
-          This project was coded by Chimdinma Egbuchulam and is{" "}
-          <a
-            href="https://github.com/didichulam/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://dinma-react-weather-app.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Netlify
-          </a>
-        </footer>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.chimdinma.com/">Chimdinma Egbuchulam</a> and is{" "}
+        <a
+          href="https://github.com/didichulam/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dinma-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
